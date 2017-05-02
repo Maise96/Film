9ad13a7238c0fdc638065b7b242b9fc869960148
Monster <bob4thegirl2014@gmail.com>
2017-05-02 19:59:31 +0200

    indset film datalaget virker

--- FilmSorting/src/data/InsertDataFilm.java
+++ FilmSorting/src/data/InsertDataFilm.java
@@ -6,32 +6,31 @@ import java.sql.SQLException;
 import domain.DomainClassFilm;
 
 public class InsertDataFilm {
-	
-	public DomainClassFilm opretEnFilm (DomainClassFilm domain){
-		try (DataAccess access = new DataAccess()){
+
+	public DomainClassFilm opretEnFilm(DomainClassFilm domain) {
+		try (DataAccess access = new DataAccess()) {
 			try {
 				opretEnFilm(access, domain);
 				access.commit();
-			} catch (Exception e){
+			} catch (Exception e) {
 				access.rollback();
 				throw e;
 			}
-		} 
+		}
 		return domain;
 	}
 
 	public DomainClassFilm opretEnFilm(DataAccess dataAccess, DomainClassFilm domainClassFilm) {
-		try (PreparedStatement statement = dataAccess.getConnection().prepareStatement(
-				"INSERT INTO film (navn, name, aarstal, audio, sub) VALUES (?, ?, ?, ?, ?)")){
+		try (PreparedStatement statement = dataAccess.getConnection()
+				.prepareStatement("INSERT INTO film (navn, name, aarstal, audio, sub) VALUES (?, ?, ?, ?, ?)")) {
 			statement.setString(1, domainClassFilm.getNavn());
 			statement.setString(2, domainClassFilm.getName());
 			statement.setString(3, domainClassFilm.getAarstal());
 			statement.setString(4, domainClassFilm.getAudio());
 			statement.setString(5, domainClassFilm.getSub());
-			int antal = statement.executeUpdate();
-			System.out.println("Antal rækker berørt: " + antal + " Film tilføjet");
+			statement.execute();
 			
-		}catch (SQLException e){
+		} catch (SQLException e) {
 			throw new RuntimeException("Noget gik galt RuntimeException", e);
 		}
 		return domainClassFilm;
