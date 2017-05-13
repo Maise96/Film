802dd0b25db450324694c7236cbfee120cfdf7bd
Monster <bob4thegirl2014@gmail.com>
2017-05-13 11:51:51 +0200

    tilføjet indsætelse af note, sub og audio

--- FilmSorting/src/data/InsertDataSeries.java
+++ FilmSorting/src/data/InsertDataSeries.java
@@ -21,15 +21,18 @@ public class InsertDataSeries {
 
 	public DomainClassSeries tilfojEnSerie(DataAccess dataAccess, DomainClassSeries domainClassSerie) {
 		try (PreparedStatement statement = dataAccess.getConnection()
-				.prepareStatement("INSERT INTO serie (navn, name, season, aarstal) VALUES (?, ?, ?, ?)")) {
+				.prepareStatement("INSERT INTO serie (navn, name, season, aarstal, audio, sub, note) VALUES (?, ?, ?, ?, ?, ?, ?)")) {
 			statement.setString(1, domainClassSerie.getNavn());
 			statement.setString(2, domainClassSerie.getName());
-			statement.setString(3, domainClassSerie.getSeason());
+			statement.setInt(3, domainClassSerie.getSeason());
 			statement.setString(4, domainClassSerie.getAarstal());
+			statement.setString(5, domainClassSerie.getAudio());
+			statement.setString(6, domainClassSerie.getSub());
+			statement.setString(7, domainClassSerie.getNote());
 			statement.execute();
 
 		} catch (SQLException e) {
-			throw new RuntimeException("Noget gik galt RuntimeException", e);
+			throw new RuntimeException("Noget gik galt InsertSerieData", e);
 		}
 		return domainClassSerie;
 	}
