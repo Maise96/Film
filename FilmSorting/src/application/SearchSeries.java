1d9b9f44c8ed081243dc132fc7f33cc674ab2401
Monster <bob4thegirl2014@gmail.com>
2017-05-14 09:59:21 +0200

    Css for søgning i serier done

--- FilmSorting/src/application/SearchSeries.java
+++ FilmSorting/src/application/SearchSeries.java
@@ -16,7 +16,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-import logic.FilmSortImpl;
+import logic.SeriesSortImpl;
 
 public class SearchSeries {
 
@@ -30,16 +30,17 @@ public class SearchSeries {
 		searchSeries.setResizable(false);
 		BorderPane border = new BorderPane();
 
-		Label sogLabel = new Label("Søg på den Danske titel\n eller den Engelske titel.");
-		sogLabel.setId("sogLabel");
+		SeriesSortImpl sog = new SeriesSortImpl();
+		serieliste = sog.sogSeriesListe("");
+		observableListSogSerie = FXCollections.observableArrayList(serieliste);
+		table.setItems(observableListSogSerie);
+
+		Label sogLabel = new Label("Søg på den Danske \ntitel eller den \nEngelske titel.");
 		TextField sogTextField = new TextField();
-		sogTextField.setId("sogTextField");
 		sogTextField.setOnKeyReleased(e -> {
 			String soog = sogTextField.getText();
-
-			FilmSortImpl logicSog = new FilmSortImpl();
+			SeriesSortImpl logicSog = new SeriesSortImpl();
 			serieliste = logicSog.sogSeriesListe(soog);
-
 			observableListSogSerie = FXCollections.observableArrayList(serieliste);
 			table.setItems(observableListSogSerie);
 			searchSeries.show();
@@ -47,16 +48,17 @@ public class SearchSeries {
 
 		Button tilbageknap = new Button("Tilbage");
 		tilbageknap.setOnAction(e -> {
-			SeriesMenu seriesMenu = new SeriesMenu();
-			seriesMenu.start(new Stage());
+			MenuSeries menuSeries = new MenuSeries();
+			menuSeries.start(new Stage());
 			searchSeries.close();
 		});
 
+		HBox vbox = new HBox();
+		vbox.getChildren().addAll(sogTextField, tilbageknap);
 		HBox knapperhbox = new HBox();
 		knapperhbox.setSpacing(9);
 		knapperhbox.setAlignment(Pos.BOTTOM_LEFT);
-		knapperhbox.getChildren().addAll(sogLabel, sogTextField, tilbageknap);
-		border.setTop(knapperhbox);
+		knapperhbox.getChildren().addAll(sogLabel, vbox);
 
 		TableColumn<DomainClassSeries, String> navn = new TableColumn<DomainClassSeries, String>("Dansk Titel");
 		navn.setCellValueFactory(new PropertyValueFactory<DomainClassSeries, String>("navn"));
@@ -77,10 +79,10 @@ public class SearchSeries {
 		season.prefWidthProperty().bind(table.widthProperty().multiply(0.05));
 		audio.prefWidthProperty().bind(table.widthProperty().multiply(0.229));
 		sub.prefWidthProperty().bind(table.widthProperty().multiply(0.229));
-
 		table.getColumns().addAll(season, navn, name, aarstal, audio, sub);
-		table.autosize();
+
 		border.setBottom(table);
+		border.setTop(knapperhbox);
 
 		Scene scene = new Scene(border, 1200, 680);
 		scene.getStylesheets().add(Main.class.getResource("searchSeries.css").toExternalForm());
