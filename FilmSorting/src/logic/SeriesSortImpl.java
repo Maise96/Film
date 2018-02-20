aabba4728f1e61936c13901350e0675c3f53e04c
Monster <bob4thegirl2014@gmail.com>
2018-02-20 13:14:16 +0100

    Logikken for serier

--- FilmSorting/src/logic/SeriesSortImpl.java
+++ FilmSorting/src/logic/SeriesSortImpl.java
@@ -7,23 +7,24 @@ import data.InsertDataSeries;
 import data.SearchSeriesData;
 import domain.DomainClassSeries;
 
-public class SeriesSortImpl implements SeriesSortInterface{
+public class SeriesSortImpl implements SeriesSortInterface {
 	private SearchSeriesData ssd = new SearchSeriesData();
 	private InsertDataSeries ts = new InsertDataSeries();
 	private ChangeSeriesData rs = new ChangeSeriesData();
-	
+
 	@Override
 	public List<DomainClassSeries> sogSeriesListe(String soog) {
 		return ssd.sogSeriesListe(soog);
 	}
-	
+
 	@Override
-	public DomainClassSeries tilfojEnSerie(DomainClassSeries domain){
+	public DomainClassSeries tilfojEnSerie(DomainClassSeries domain) {
 		ts.tilfojEnSerie(domain);
 		return domain;
 	}
+
 	@Override
-	public DomainClassSeries redigerSerie(DomainClassSeries domain){
+	public DomainClassSeries redigerSerie(DomainClassSeries domain) {
 		rs.redigerSerie(domain);
 		return domain;
 	}
--- FilmSorting/src/logic/SeriesSortInterface.java
+++ FilmSorting/src/logic/SeriesSortInterface.java
@@ -6,6 +6,8 @@ import domain.DomainClassSeries;
 
 public interface SeriesSortInterface {
 	List<DomainClassSeries> sogSeriesListe(String soog);
+
 	DomainClassSeries tilfojEnSerie(DomainClassSeries domain);
+
 	DomainClassSeries redigerSerie(DomainClassSeries domain);
 }
