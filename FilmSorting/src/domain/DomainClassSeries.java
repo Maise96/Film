8f067adc00696848ab85f3f7a2470ffaef0079fe
Monster <bob4thegirl2014@gmail.com>
2018-02-20 13:45:50 +0100

    Er rettet domain for serie

--- FilmSorting/src/domain/DomainClassSeries.java
+++ FilmSorting/src/domain/DomainClassSeries.java
@@ -9,52 +9,67 @@ public class DomainClassSeries {
 	private String audio;
 	private String sub;
 	private String note;
-	
+
 	public int getRefs() {
 		return refs;
 	}
+
 	public void setRefs(int refs) {
 		this.refs = refs;
 	}
+
 	public String getNavn() {
 		return navn;
 	}
+
 	public void setNavn(String navn) {
 		this.navn = navn;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getAarstal() {
 		return aarstal;
 	}
+
 	public void setAarstal(String aarstal) {
 		this.aarstal = aarstal;
 	}
+
 	public int getSeason() {
 		return season;
 	}
+
 	public void setSeason(int season) {
 		this.season = season;
 	}
+
 	public String getAudio() {
 		return audio;
 	}
+
 	public void setAudio(String audio) {
 		this.audio = audio;
 	}
+
 	public String getSub() {
 		return sub;
 	}
+
 	public void setSub(String sub) {
 		this.sub = sub;
 	}
+
 	public String getNote() {
 		return note;
 	}
+
 	public void setNote(String note) {
 		this.note = note;
 	}
