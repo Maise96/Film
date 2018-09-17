e6dc6dc3816726de1ec05b7a02906a3997322041
Monster <bob4thegirl2014@gmail.com>
2018-09-17 09:09:49 +0200

    Domainklasse oprettet

---
+++ FilmSorting/src/domain/DomainClassFilmSeries.java
@@ -0,0 +1,230 @@
+package domain;
+
+public class DomainClassFilmSeries {
+	private int reff, refs;
+	private String navnf, navns;
+	private String namef, names;
+	private String audiof, audios;
+	private String subf, subs;
+	private boolean blurayf, blurays;
+	private String yearf, years;
+	private boolean burned;
+	private boolean kids;
+	private boolean animation;
+	private boolean danish;
+	private boolean horror;
+	private String notef, notes;
+	private int cs;
+	private int season;
+	private int number;
+	private int episode;
+	private int volume;
+//getters og setters
+	public int getReff() {
+		return reff;
+	}
+
+	public void setReff(int reff) {
+		this.reff = reff;
+	}
+
+	public int getRefs() {
+		return refs;
+	}
+
+	public void setRefs(int refs) {
+		this.refs = refs;
+	}
+
+	public String getNavnf() {
+		return navnf;
+	}
+
+	public void setNavnf(String navnf) {
+		this.navnf = navnf;
+	}
+
+	public String getNavns() {
+		return navns;
+	}
+
+	public void setNavns(String navns) {
+		this.navns = navns;
+	}
+
+	public String getNamef() {
+		return namef;
+	}
+
+	public void setNamef(String namef) {
+		this.namef = namef;
+	}
+
+	public String getNames() {
+		return names;
+	}
+
+	public void setNames(String names) {
+		this.names = names;
+	}
+
+	public String getAudiof() {
+		return audiof;
+	}
+
+	public void setAudiof(String audiof) {
+		this.audiof = audiof;
+	}
+
+	public String getAudios() {
+		return audios;
+	}
+
+	public void setAudios(String audios) {
+		this.audios = audios;
+	}
+
+	public String getSubf() {
+		return subf;
+	}
+
+	public void setSubf(String subf) {
+		this.subf = subf;
+	}
+
+	public String getSubs() {
+		return subs;
+	}
+
+	public void setSubs(String subs) {
+		this.subs = subs;
+	}
+
+	public boolean isBlurayf() {
+		return blurayf;
+	}
+
+	public void setBlurayf(boolean blurayf) {
+		this.blurayf = blurayf;
+	}
+
+	public boolean isBlurays() {
+		return blurays;
+	}
+
+	public void setBlurays(boolean blurays) {
+		this.blurays = blurays;
+	}
+
+	public String getYearf() {
+		return yearf;
+	}
+
+	public void setYearf(String yearf) {
+		this.yearf = yearf;
+	}
+
+	public String getYears() {
+		return years;
+	}
+
+	public void setYears(String years) {
+		this.years = years;
+	}
+
+	public boolean isBurned() {
+		return burned;
+	}
+
+	public void setBurned(boolean burned) {
+		this.burned = burned;
+	}
+
+	public boolean isKids() {
+		return kids;
+	}
+
+	public void setKids(boolean kids) {
+		this.kids = kids;
+	}
+
+	public boolean isAnimation() {
+		return animation;
+	}
+
+	public void setAnimation(boolean animation) {
+		this.animation = animation;
+	}
+
+	public boolean isDanish() {
+		return danish;
+	}
+
+	public void setDanish(boolean danish) {
+		this.danish = danish;
+	}
+
+	public boolean isHorror() {
+		return horror;
+	}
+
+	public void setHorror(boolean horror) {
+		this.horror = horror;
+	}
+
+	public String getNotef() {
+		return notef;
+	}
+
+	public void setNotef(String notef) {
+		this.notef = notef;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public int getCs() {
+		return cs;
+	}
+
+	public void setCs(int cs) {
+		this.cs = cs;
+	}
+
+	public int getSeason() {
+		return season;
+	}
+
+	public void setSeason(int season) {
+		this.season = season;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public int getEpisode() {
+		return episode;
+	}
+
+	public void setEpisode(int episode) {
+		this.episode = episode;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+}
